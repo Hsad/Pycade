0e03365284395c7aab24fa66a00a4466d77705f0
Parshva Shah <g7parsh@gmail.com>
2014-09-17 14:14:10 -0400

    Finished "Cutscene"

--- src/Cutscenes.py
+++ src/Cutscenes.py
@@ -8,7 +8,13 @@ class Cutscenes(object):
 		self.current_state = "Team Logo"
 		self.font= pygame.font.Font(None,20)
 		self.text = self.font.render("", 0, pygame.Color("red"), pygame.Color("black"))
-      	
+		self.text2 = self.font.render("", 0, pygame.Color("red"), pygame.Color("black"))
+		self.text3 = self.font.render("", 0, pygame.Color("red"), pygame.Color("black"))
+
+		self.text4 = self.font.render("", 0, pygame.Color("red"), pygame.Color("black"))
+
+		self.text5 = self.font.render("", 0, pygame.Color("red"), pygame.Color("black"))
+		
 		
 		#Team Logo Animation Pieces
 		self.logo_animation_start = pygame.image.load("../assets/Art/Logo_Entrence_Animation.png").convert_alpha()
@@ -17,6 +23,7 @@ class Cutscenes(object):
 		
 		self.menu_background = pygame.image.load("../assets/Art/Castle_Background.png").convert_alpha()
 		self.player = Player.Player(self.screen)
+		self.player.state = 1
 		self.player.rect.left = self.screen.get_rect().width/2+100
 		self.player.rect.bottom = self.screen.get_rect().height
 		self.knight_image = pygame.image.load("../assets/Art/knight_walk.png")
@@ -64,11 +71,18 @@ class Cutscenes(object):
 			if self.current_state == "Conversation":
 				self.screen.blit(self.menu_background, pygame.Rect(0, 0, 800, 600))
 				self.screen.blit(self.knight_image, self.knight_rect, pygame.Rect(64, 0, 64, 80))
+				self.screen.blit(self.player.image, self.player.rect, pygame.Rect(64*(self.player.frame), (2*self.player.state + 1) * 80 , 64, 80))
+				#self.player.draw(self.screen)
 				
-				print self.player.rect.x
-				self.player.draw(self.screen)
-				self.text = self.font.render("Help, the prince has been taken!", 0, pygame.Color("red"), pygame.Color("black"))
-      			self.screen.blit(self.text, pygame.Rect(250,250, 10,10))
-		
-			
+				
+				self.text = self.font.render("I understand your concern Princess, but unfortunately I cannot oblige.", 0, pygame.Color("red"), pygame.Color("black"))
+				self.screen.blit(self.text, pygame.Rect(250,250, 10,10))
+				self.text2 = self.font.render("The knights who have captured your brother are known for their viciousness", 0, pygame.Color("red"), pygame.Color("black"))
+				self.screen.blit(self.text2, pygame.Rect(250,265, 10,10))
+				self.text3 = self.font.render("and it would not be fitting of me as a protector of hte royal family if I", 0, pygame.Color("red"), pygame.Color("black"))
+				self.screen.blit(self.text3, pygame.Rect(250,280, 10,10))				
+				self.text4 = self.font.render("allowed you to put yourself in harm's way.", 0, pygame.Color("red"), pygame.Color("black"))
+				self.screen.blit(self.text4, pygame.Rect(250,295, 10,10))	
+				self.text5 = self.font.render("Do not worry.  I assure you I will see to the prince's swift return.", 0, pygame.Color("green"), pygame.Color("black"))
+				self.screen.blit(self.text5, pygame.Rect(250,310, 10,10))
 			pygame.display.flip()
